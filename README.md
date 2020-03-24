a9812407f60ed098027610fcf3fb65f06c58b247
yjh126yjh <yjh126yjh@example.com>
2020-03-24 22:12:08 +0800

    Add trained weight file

--- README.md
+++ README.md
@@ -2,11 +2,16 @@
 ## 比赛题目
 - 目前已知一部分氨基酸序列和与其对应的二级结构，通过已有数据寻找一级结构到二级结构的映射模型，提高通过氨基酸序列进行蛋白质二级结构预测的准确性。
 - 比赛链接：[蛋白质结构预测大赛-天池大赛-阿里云天池](https://tianchi.aliyun.com/competition/entrance/231781/information)
+- 天池Notebook：[蛋白质结构预测大赛TOP3团队方案分享-天池实验室-阿里云天池](https://tianchi.aliyun.com/notebook-ai/detail?postId=98092)
 ## 文件说明
-- model4.py：定义并生成模型
-- evaluate4.py：加载训练得到的参数并进行二级结构预测
+- model.py：定义并生成模型
+- evaluate.py：加载训练得到的参数并进行二级结构预测
+- whole_sequence-best.hdf5.xz：已经训练好的网络参数，线上成绩0.772
 ## 方案介绍
 ### 特征表示
-![avatar](https://github.com/yjh126yjh/TianChi_Protein-Secondary-Structure-Prediction/raw/master/pics/Feature_Representation.png)
+![](https://github.com/yjh126yjh/TianChi_Protein-Secondary-Structure-Prediction/raw/master/pics/Feature_Representation.png)
 ### 网络结构
-![avatar](https://github.com/yjh126yjh/TianChi_Protein-Secondary-Structure-Prediction/raw/master/pics/CNN.png)
+![](https://github.com/yjh126yjh/TianChi_Protein-Secondary-Structure-Prediction/raw/master/pics/CNN.png)
+### 结果复现
+1. 下载model.py、evaluate.py，下载whole_sequence-best.hdf5.xz并解压，放在同一个文件夹下。
+2. 将evaluate.py中的filepath改成测试数据的路径，然后执行python evaluate.py。
---
+++ evaluate.py
@@ -0,0 +1,82 @@
+import numpy as np
+import model
+
+# onehot for 21 amino acid & features
+seqdict = {
+    "G": [[0, 22, 30, 34], -0.08, -0.88],
+    "P": [[1, 26, 30, 34], -0.32, -0.31],
+    "T": [[2, 27, 31, 34], -0.14, -0.25],
+    "E": [[3, 21, 29, 33], -0.7, 0.15],
+    "S": [[4, 27, 31, 34], -0.16, -0.45],
+    "K": [[5, 25, 32, 35], -0.78, 0.13],
+    "C": [[6, 28, 30, 34], 0.5, -0.22],
+    "L": [[7, 22, 30, 34], 0.76, -0.08],
+    "M": [[8, 28, 30, 34], 0.38, 0.18],
+    "V": [[9, 22, 30, 34], 0.84, -0.28],
+    "D": [[10, 21, 29, 33], -0.7, -0.05],
+    "A": [[11, 22, 30, 34], 0.36, -0.68],
+    "R": [[12, 25, 32, 35], -0.9, 0.53],
+    "I": [[13, 22, 30, 34], 0.9, -0.08],
+    "N": [[14, 23, 31, 34], -0.7, -0.07],
+    "H": [[15, 24, 25, 32, 33], -0.64, 0.26],
+    "F": [[16, 24, 30, 34], 0.56, 0.40],
+    "W": [[17, 24, 30, 34], -0.18, 0.96],
+    "Y": [[18, 24, 31, 34], -0.26, 0.63],
+    "Q": [[19, 23, 31, 34], -0.7, 0.13],
+    "X": [[20], -0.04, 0]
+}
+
+# onehot for 8 structure
+secdict = {
+    " ": 0,
+    "E": 1,
+    "T": 2,
+    "S": 3,
+    "H": 4,
+    "G": 5,
+    "B": 6,
+    "I": 7,
+}
+table = [' ', 'E', 'T', 'S', 'H', 'G', 'B', 'I']
+
+filepath = r"test.txt"  # <---- filepath of amino acid sequence
+n = len(open(filepath, 'r').readlines())
+
+X_test = np.zeros((n, 700, 38))
+Y_test = np.zeros((n, 700, 1))
+i_num = 0
+
+# preprocess
+with open(filepath) as f:
+    for lines in f:
+        i_char = 0
+        for char in lines[:-1]:
+            indexlist = seqdict.get(char)
+            if indexlist == None:
+                indexlist = seqdict.get("X")
+            for index in indexlist[0]:
+                X_test[i_num][i_char][index] = 1
+            X_test[i_num][i_char][36] = indexlist[1]
+            X_test[i_num][i_char][37] = indexlist[2]
+            Y_test[i_num][i_char][0] = 1
+            i_char += 1
+        i_num += 1
+
+net = model.CNN_model()
+
+# load Weights
+net.load_weights(r"whole_sequence-best.hdf5")
+
+predictions = net.predict(X_test)
+
+# postprocess and output results
+with open("result.txt", 'w') as f:
+    for i in range(Y_test.shape[0]):  # process each sequence
+        line = ""
+        for j in range(Y_test.shape[1]):  # process each character of the sequence
+            if np.sum(Y_test[i, j, :]) == 0:  # np.sum(Y_test[i, j, :]) == 0 if it is padding
+                break
+            index = np.argmax(predictions[i, j, :])
+            line += table[index]
+        line += "\n"
+        f.write(line)
